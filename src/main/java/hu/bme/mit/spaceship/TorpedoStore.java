b8c9d8c7b1284fcb7d43e73cebf7c88a226ea96c
Marosi Milán <84237262+marosimilan@users.noreply.github.com>
2025-10-15 16:37:02 +0200

    ez igen baratom!

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,6 +30,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
+      //ez igen barátom!
       throw new IllegalArgumentException("numberOfTorpedos");
     }
 
